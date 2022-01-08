e235fb8be126d48bc5aa842111c47ee298e97b9f
Wadim Skworzow <sempris2008@gmail.com>
2022-01-08 14:33:19 +0100

    testing section added

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Footer from "./Footer";
 
 function Maps() {
-    return (
-        <div>
-            
+  const [mapsDistrictData, setMapsDistrictData] = useState('');
+
+  const getMapsDistrictData = async () => {
+    const data = await (
+      await fetch("https://api.corona-zahlen.org/map/districts-legend")
+    ).json();
+    setMapsDistrictData(data);
+  };
+
+  useEffect(() => {
+    getMapsDistrictData();
+  }, []);
+
+  return (
+    <div>
+      <h1>COVID-19 in Germany</h1>
+      <h2>Information on Maps</h2>
+      {mapsDistrictData ? (
+        <div className="container main-box">
+          <div className="map"><img src={mapsDistrictData} alt="District Data"/></div>
         </div>
-    )
+      ) : (
+        <p>no data available</p>
+      )}
+      <footer>
+        <Footer/>
+      </footer>
+    </div>
+  );
 }
 
-export default Maps
+export default Maps;
--- src/components/Testing.jsx
+++ src/components/Testing.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Footer from "./Footer";
 
 function Testing() {
-    return (
-        <div>
-            
+  const [testingData, setTestingData] = useState({});
+
+  const getTestingData = async () => {
+    const data = await (
+      await fetch("https://api.corona-zahlen.org/testing/history")
+    ).json();
+    setTestingData(data);
+  };
+
+  useEffect(() => {
+    getTestingData();
+  }, []);
+
+  return (
+    <div>
+      <h1>COVID-19 in Germany</h1>
+      <h2>Information about Tests</h2>
+      {/* {testingData ? (
+        <div className="container test-box">
+          <div className="box">
+            Performed Tests:{" "}
+            <span className="bold">
+              {testingData.data.history.performedTests && testingData.data.history.performedTests.toLocaleString()}
+            </span>
+          </div>
+          <div className="box">
+            Positive Tests:{" "}
+            <span className="bold">
+              {testingData.data.history.positiveTests && testingData.data.history.positiveTests.toLocaleString()}
+            </span>
+          </div>
+          <div className="box">
+            Positive Test Rate:{" "}
+            <span className="bold">
+              {testingData.data.history.positivityRate && testingData.data.history.positivityRate.toLocaleString()}
+            </span>
+          </div>
         </div>
-    )
+      ) : (
+        <p>no data available</p>
+      )} */}
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
-export default Testing
+export default Testing;
